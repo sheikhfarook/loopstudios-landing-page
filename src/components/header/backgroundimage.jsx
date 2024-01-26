ca03d189956753746c754e6df451ddf2dc3a35df
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-01-26 17:16:15 +0530

    create the new components

---
+++ src/components/header/backgroundimage.jsx
@@ -0,0 +1,12 @@
+import hero from "../../assets/images/image-hero.jpg";
+const BackGroundImage = () => {
+  return (
+    <div>
+      <div className="hero">
+        <img src={hero} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default BackGroundImage;
