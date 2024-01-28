5fdd4720c2443509a399b45f8200ba2c79d82dcd
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-01-28 16:40:37 +0530

    create the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import SocialCommunity from "./components/footer/socialcommunity";
 import BackGroundImage from "./components/header/backgroundimage";
+import OurCreations from "./components/main/creation/ourcreations";
+// import OurCreations from "./components/main/creation/ourcreations";
+import VrInFo from "./components/main/vrinfo";
+import { TailwindIndicator } from "./components/tailwind/tailwindindicator";
 
 function App() {
   return (
     <div>
       <BackGroundImage />
+      <VrInFo />
     </div>
   );
 }
---
+++ src/components/main/creation/ourcreations.jsx
@@ -0,0 +1,32 @@
+import Imges from "./imges";
+
+const OurCreations = () => {
+  return (
+    <div className=" mt-[9%]">
+      <div className="mx-[12%] flex justify-between">
+        <div
+          className="text-[#000] text-[3rem] font-[300] font-josefin leading-[3rem] uppercase 
+        max-sm:text-[2rem] max-sm:font-[300] max-sm:leading-[2rem] ">
+          OUR CREATIONS
+        </div>
+        <div
+          className="w-[9.8125rem] h-[2.5rem] hover:bg-[#000] hover:text-[#FFF]
+         border-2 border-[#000] text-[#000]  cursor-pointer font-alata fonr-[400] 
+          uppercase pt-2.5 text-center leading-[0.875rem] tracking-[0.3125rem] max-sm:hidden">
+          SEE ALL
+        </div>
+      </div>
+      <div className="mt-[5%]">
+        <Imges />
+      </div>
+      <div
+        className="w-[9.8125rem] h-[2.5rem] m-auto font-alata fonr-[400]
+         border-2 border-[#000] pt-2.5 text-[#000] text-center cursor-pointer  
+          uppercase leading-[0.875rem] tracking-[0.3125rem] hover:bg-[#000] hover:text-[#FFF]  md:hidden">
+        SEE ALL
+      </div>
+    </div>
+  );
+};
+
+export default OurCreations;
