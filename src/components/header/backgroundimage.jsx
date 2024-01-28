b841b22f1a49775e98de3f5ec2b9db30c5ff65d4
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-01-28 16:36:22 +0530

    import the image

--- src/components/header/backgroundimage.jsx
+++ src/components/header/backgroundimage.jsx
@@ -1,12 +1,15 @@
 import hero from "../../assets/images/image-hero.jpg";
+import hero2 from "../../assets/mobile/image-hero.jpg";
 import Header from "./header";
 const BackGroundImage = () => {
   return (
-    <div>
+    <div className="">
       <div>
-        <img className="w-[100%] h-[100%]" src={hero} alt="" />
+        <img className="w-[100%] h-[100%] max-sm:hidden" src={hero} alt="" />
+        <img className="w-[100%] h-[100%] md:hidden" src={hero2} alt="" />
       </div>
       <div>
+        {" "}
         <Header />
       </div>
     </div>
