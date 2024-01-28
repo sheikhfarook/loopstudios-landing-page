0c5f50d1703de6481c03f0683a0eaf493bd34843
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-01-28 16:41:36 +0530

    fix the code

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,12 +2,19 @@ import logo from "../../assets/logo.svg";
 
 const Header = () => {
   return (
-    <div className="mt-[-40.5%] mx-[12%] space-y-[11%]">
-      <div className="flex   justify-between">
+    <div className="md:mt-[-40.5%] md:mx-[12%] md:space-y-[11%] max-sm:mt-[-165%] max-sm:mx-[7%] max-sm:space-y-[45%]">
+      <div className="flex justify-between">
         <div>
-          <img src={logo} alt="" />
+          <img
+            className="max-sm:w-[9rem] max-sm:h-[1.5rem]"
+            src={logo}
+            alt=""
+          />
         </div>
-        <div className="flex gap-[2rem] hover:underline-offset-8 text-[0.9375rem] font-[400] text-[#FFF] ">
+        <div
+          className="flex gap-[2rem] hover:underline-offset-8 text-[0.9375rem] 
+        font-[400] text-[#FFF] font-alata my-auto
+        max-sm:hidden">
           <div className="cursor-pointer hover:underline decoration-2">
             <div className="rr">About</div>
           </div>
@@ -25,8 +32,17 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <div className="w-[40.625rem] h-[17.375rem] border-2 relative border-white ">
-        <p className="w-[35.625rem] h-[12.375rem] m-auto my-7  text-[4.5rem] text-[#FFF] font-[300] leading-[4.375rem] uppercase">
+      <div
+        className="border-2 relative border-white font-josefin xl:w-[40.625rem] xl:h-[17.375rem] 
+        lg:w-[28.625rem] lg:h-[13.375rem]
+        md:w-[21.625rem] md:h-[10.375rem]
+        max-sm:w-[20.4375rem] max-sm:h-[12.25rem] ">
+        <p
+          className="m-auto xl:text-[4.5rem] text-[#FFF] font-[300]  uppercase  
+        xl:my-7 xl:w-[35.625rem] xl:h-[12.375rem] xl:leading-[4.375rem] 
+        lg:leading-[3.5rem] lg:text-[3.25rem] lg:my-5 lg:w-[25.625rem]  lg:h-[12.375rem] 
+        md:leading-[2.7rem] md:text-[2.5rem] md:my-3.5 md:w-[18.955rem]  md:h-[10.375rem] 
+        max-sm:leading-[2.375rem] max-sm:text-[2.5rem] max-sm:my-3 max-sm:w-[17.4375rem]  max-sm:h-[9.125rem]">
           Immersive experiences that deliver
         </p>
       </div>
