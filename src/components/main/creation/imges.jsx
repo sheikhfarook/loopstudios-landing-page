30701978b9ac1d86956a8e5c513cb9e868ff4609
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-01-29 10:53:22 +0530

    complete the task

--- src/components/main/creation/imges.jsx
+++ src/components/main/creation/imges.jsx
@@ -19,28 +19,28 @@ const Imges = () => {
   return (
     <div>
       <div className="grid font-josefin xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 w-[79%] mx-[11.5%] ">
-        <div className="md:mb-32 hover:opacity-25 cursor-pointer max-sm:mb-16 ">
+        <div className="md:mb-32 hover:opacity-25 cursor-pointer max-sm:mb-10 ">
           <img className="max-sm:hidden" src={EARTH} alt="" />
           <img className="md:hidden h-[7.5rem]" src={Earth} alt="" />
 
           <p
             className=" w-[6rem] text-[2rem] font-[300] leading-[2rem]  
           text-[#FFF] relative ml-[2.5rem] mt-[-6.4rem]
-          max-sm:text-[1.5rem] max-sm:leading-[1.5rem] max-sm:ml-[0.89rem] max-sm:mt-[-4.2rem]">
+          max-sm:text-[1.5rem] max-sm:w-[5rem] max-sm:leading-[1.5rem] max-sm:ml-[0.89rem] max-sm:mt-[-4.2rem]">
             DEEP EARTH
           </p>
         </div>
-        <div className=" mb-32 hover:opacity-25 cursor-pointer max-sm:mb-16">
+        <div className=" mb-32 hover:opacity-25 cursor-pointer max-sm:mb-10">
           <img className="max-sm:hidden" src={ARCADE} alt="" />
           <img className="md:hidden h-[7.5rem]" src={Arcade} alt="" />
 
           <p
             className="w-[6rem] text-[2rem] font-[300] leading-[2rem] text-[#FFF]  relative  ml-[2.5rem] mt-[-6.4rem]
-                    max-sm:text-[1.5rem] max-sm:leading-[1.5rem] max-sm:ml-[0.89rem] max-sm:mt-[-4.2rem]">
+                    max-sm:text-[1.5rem] max-sm:w-[6.5rem] max-sm:leading-[1.5rem] max-sm:ml-[0.89rem] max-sm:mt-[-4.2rem]">
             NIGHT ARCADE
           </p>
         </div>
-        <div className="mb-32 hover:opacity-25 cursor-pointer max-sm:mb-16">
+        <div className="mb-32 hover:opacity-25 cursor-pointer max-sm:mb-10">
           <img className="max-sm:hidden" src={SOCCER} alt="" />
           <img className="md:hidden h-[7.5rem]" src={Soccer} alt="" />
 
@@ -51,7 +51,7 @@ const Imges = () => {
             SOCCER TEAM VR
           </p>
         </div>
-        <div className="mb-32 hover:opacity-25 cursor-pointer max-sm:mb-16">
+        <div className="mb-32 hover:opacity-25 cursor-pointer max-sm:mb-10">
           <img className="max-sm:hidden" src={GRID} alt="" />
           <img className="md:hidden h-[7.5rem]" src={Grid} alt="" />
 
@@ -61,7 +61,7 @@ const Imges = () => {
             THE GRID
           </p>
         </div>
-        <div className="mb-32 hover:opacity-25 cursor-pointer max-sm:mb-16">
+        <div className="mb-32 hover:opacity-25 cursor-pointer max-sm:mb-10">
           <img className="max-sm:hidden" src={ABOVE} alt="" />
           <img className="md:hidden h-[7.5rem]" src={Above} alt="" />
 
@@ -71,27 +71,27 @@ const Imges = () => {
             FROM UP ABOVE VR
           </p>
         </div>
-        <div className="mb-32  hover:opacity-25 cursor-pointer max-sm:mb-16">
+        <div className="mb-32  hover:opacity-25 cursor-pointer max-sm:mb-10">
           <img className="max-sm:hidden" src={BOREALIS} alt="" />
           <img className="md:hidden h-[7.5rem]" src={Borealis} alt="" />
 
           <p
             className="w-[6rem] text-[2rem] font-[300] leading-[2rem] text-[#FFF]   relative  ml-[2.5rem] mt-[-6.4rem]
-                    max-sm:text-[1.5rem] max-sm:leading-[1.5rem] max-sm:ml-[0.89rem] max-sm:mt-[-4.2rem]">
+            max-sm:w-[7.7rem]       max-sm:text-[1.5rem] max-sm:leading-[1.5rem] max-sm:ml-[0.89rem] max-sm:mt-[-4.2rem]">
             POCKET BOREALIS
           </p>
         </div>
-        <div className=" mb-32  hover:opacity-25 cursor-pointer max-sm:mb-16 ">
+        <div className=" mb-32  hover:opacity-25 cursor-pointer max-sm:mb-10 ">
           <img className="max-sm:hidden" src={CURIOSITY} alt="" />
           <img className="md:hidden h-[7.5rem]" src={Curiosity} alt="" />
 
           <p
             className="w-[6rem]  text-[2rem] font-[300] leading-[2rem] text-[#FFF]  relative  ml-[2.5rem] mt-[-6.4rem]
-                    max-sm:text-[1.5rem] max-sm:leading-[1.5rem] max-sm:ml-[0.89rem] max-sm:mt-[-4.2rem]">
+            max-sm:w-[7.9rem]       max-sm:text-[1.5rem] max-sm:leading-[1.5rem] max-sm:ml-[0.89rem] max-sm:mt-[-4.2rem]">
             THE CURIOSITY
           </p>
         </div>
-        <div className="mb-32  hover:opacity-25 cursor-pointer max-sm:mb-16">
+        <div className="mb-32  hover:opacity-25 cursor-pointer max-sm:mb-10">
           <img className="max-sm:hidden" src={FISHEYE} alt="" />
           <img className="md:hidden h-[7.5rem]" src={Fisheye} alt="" />
 
